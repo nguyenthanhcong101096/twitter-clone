b7debbdcae3df9078a50aaf7a8385b9ce75c161a
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2021-06-05 18:56:10 +0700

    update

--- src/Feed.js
+++ src/Feed.js
@@ -24,18 +24,16 @@ function Feed() {
 
             <TweetBox />
             <FlipMove>
-                {posts.map(post => (
-                <Post 
-                key = {post.text}
-                displayName = {post.displayName}
-                username = {post.username}
-                verified = {post.verified}
-                text = {post.text}
-                image = {post.image}
-                avatar = {post.avatar}
+            <Post
+							key = {1}
+							displayName = {"Nguyễn thành cong"}
+							username = {"congnt"}
+							verified = {"true"}
+							text = {"Danh sách 10 CLB có nhiều cầu thủ chinh chiến tại Euro 2020 vắng Real Madrid, Barcelona, Liverpool hay PSG."}
+							image = {"https://picsum.photos/200/300?grayscale"}
+							avatar = {"https://res.cloudinary.com/practicaldev/image/fetch/s--4GUdCMwp--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/631770/6464230a-e07d-4a01-884f-f960378e2af1.png"}
             />
-            ))}
-            </FlipMove>   
+            </FlipMove>
         </div>
     )
 }
